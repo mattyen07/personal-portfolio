ad23ed6dcae3d49a3386d8294e4919d0eaedf16c
Matthew Yen <mgyen@shaw.ca>
2020-09-30 20:26:02 -0700

    added particles to home page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,14 +10,14 @@ const styles = makeStyles(theme => ({
     },
 
     topCardContainer: {
-      maxWidth:350,
+      maxWidth: 350,
       margin: "2rem auto",
       background:"#DCDCDC",
       boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
     },
 
     bottomCardContainer: {
-      maxWidth:350,
+      maxWidth: 350,
       margin: "2rem auto",
       background:"#DCDCDC",
       boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
--- src/Components/main.js
+++ src/Components/main.js
@@ -1,17 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavigationBar from './NavigationBar';
 import Homepage from './Homepage';
+import Particles from 'react-particles-js';
+import {makeStyles} from '@material-ui/styles';
 
+//CSS
+const styles = makeStyles(theme => ({
+    particleContainer: {
+        position: "absolute",
+        opacity: "0.65"
+    }
+}))
 
-class Main extends Component {
-    render() {
+const Main = () => {
+        const classes = styles()
         return (
             <>
                 <NavigationBar />
                 <Homepage />
+                <Particles
+                canvasClassName={classes.particleContainer}
+                params={{
+                    particles:{
+                        color: {
+                            value: "#222"
+                            },
+                            line_linked: {
+                            color: {
+                                value: "#222"
+                            }
+                            },
+                        number:{
+                            value: 60
+                        }
+                    }
+                }} />
             </>
         )
-    }
 }
 
 export default Main
